bc57c0455622884b1b209faae0a61422c9e20253
Rafael Santos <rafa.05125@gmail.com>
2023-06-08 20:21:17 -0300

    HourContract

---
+++ Department/entities/HourContract.cs
@@ -0,0 +1,23 @@
+namespace Course.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract() { }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            valuePerHour = ValuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
